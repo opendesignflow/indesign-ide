0104afc1288df14e58edb3244bfc6b2af860c2cf
richnou <leys.richard@gmail.com>
2017-03-24 20:36:31 +0100

    Added Select File utility

--- ide-core/src/main/scala/org/odfi/indesign/ide/core/ui/utils/LocalGUIExtensionsView.scala
+++ ide-core/src/main/scala/org/odfi/indesign/ide/core/ui/utils/LocalGUIExtensionsView.scala
@@ -8,9 +8,41 @@ import org.odfi.indesign.core.module.jfx.JFXRun
 import org.odfi.indesign.core.brain.Brain
 import javafx.stage.Stage
 import javafx.stage.DirectoryChooser
+import javafx.stage.FileChooser
+import javafx.stage.FileChooser.ExtensionFilter
+import org.odfi.indesign.ide.core.ui.lib.IndesignIDELibView
 
-trait LocalGUIExtensionsView extends IndesignFrameworkView {
+trait LocalGUIExtensionsView extends IndesignIDELibView {
 
+  
+  def selectFileOfTypeButton(text: String,extension:(String,String))(cl: File => Any) = {
+    "ui button" :: button(text) {
+      onClick {
+        JFXRun.onJavaFX {
+          
+          var stage = new Stage()
+          var fileChooser = new FileChooser();
+          fileChooser.setTitle("Select File...");
+          fileChooser.setSelectedExtensionFilter(new ExtensionFilter(extension._1,extension._2))
+          
+         
+          stage.setAlwaysOnTop(true)
+          //stage.show()
+          //stage.setFocused(true)
+
+          fileChooser.showOpenDialog(stage) match {
+            case null =>
+            case folder =>
+
+              cl(folder)
+
+          }
+        }
+      }
+    }
+    
+  }
+  
   def selectDirectoryButton(text: String)(cl: File => Any) = {
     "ui button" :: button(text) {
       onClick {
