801915843b47480fe48be11b59d42c015f8b199a
richnou <leys.richard@gmail.com>
2017-05-29 22:15:15 +0200

    Updated snapshot versions

--- ide-core/src/main/scala/org/odfi/indesign/ide/core/module/d3/D3View.scala
+++ ide-core/src/main/scala/org/odfi/indesign/ide/core/module/d3/D3View.scala
@@ -3,29 +3,34 @@ package org.odfi.indesign.ide.core.module.d3
 import org.odfi.indesign.ide.core.ui.lib.IndesignIDELibView
 
 trait D3View extends IndesignIDELibView {
-  
-  
+
+  var d3Modules = List("d3")
+
   this.addLibrary("d3") {
-    case (_,targetNode) => 
-      
+    case (_, targetNode) =>
+
       onNode(targetNode) {
-        
-        List("d3","d3-transition").foreach {
-          name => 
+
+        d3Modules.foreach {
+          name =>
             script(createAssetsResolverURI(s"/indesign-ide/external/node/node_modules/$name/build/$name.min.js")) {
-          
+
+            }
         }
-        }
-        
+
         /*script(createAssetsResolverURI("/indesign-ide/external/node/node_modules/d3-transition/build/d3.min.js")) {
           
         }
         script(createAssetsResolverURI("/indesign-ide/external/node/node_modules/d3/build/d3.min.js")) {
           
         }*/
-        
-        
+
       }
-      
+
   }
+
+  def d3UseZoom = {
+    d3Modules = d3Modules :+ "d3-zoom"
+  }
+
 }
--- ide-core/src/main/scala/org/odfi/indesign/ide/core/ui/lib/IndesignIDELibView.scala
+++ ide-core/src/main/scala/org/odfi/indesign/ide/core/ui/lib/IndesignIDELibView.scala
@@ -4,19 +4,19 @@ import org.odfi.wsb.fwapp.framework.FWAppFrameworkView
 import org.odfi.wsb.fwapp.assets.ResourcesAssetSource
 
 trait IndesignIDELibView extends FWAppFrameworkView {
-   
+
   this.addLibrary("indesign-ide") {
 
     case (_, targetNode) =>
 
       getAssetsResolver match {
-          case Some(a) =>
-            a.ifNoAssetSource("/indesign-ide") {
-              a.addAssetsSource("/indesign-ide", new ResourcesAssetSource).addFilesSource("indesign-ide")
-            }
-          case None => 
-        }
-      
+        case Some(a) =>
+          a.ifNoAssetSource("/indesign-ide") {
+            a.addAssetsSource("/indesign-ide", new ResourcesAssetSource).addFilesSource("indesign-ide")
+          }
+        case None =>
+      }
+
       onNode(targetNode) {
         script(createAssetsResolverURI("indesign-ide/js/indesign-ide.js")) {
 
@@ -24,5 +24,5 @@ trait IndesignIDELibView extends FWAppFrameworkView {
       }
 
   }
-  
+
 }
