5295f1843ce0cb849d78d7c440f92bc1ff9f33ca
Richard Leys <leys@witte-group.de>
2019-10-15 20:35:35 +0200

    Adding Pipeline Resource Name for Pipeline Style API

---
+++ ide-core/src/main/scala/org/odfi/indesign/ide/core/module/pipelines/Pipeline.scala
@@ -0,0 +1,12 @@
+package org.odfi.indesign.ide.core.module.pipelines
+
+import com.idyria.osi.wsb.core.broker.tree.Intermediary
+
+class Pipeline extends Intermediary {
+  
+  
+  def run = {
+    
+  }
+  
+}
---
+++ ide-core/src/main/scala/org/odfi/indesign/ide/core/module/pipelines/PipelineStep.scala
@@ -0,0 +1,8 @@
+package org.odfi.indesign.ide.core.module.pipelines
+
+import com.idyria.osi.wsb.core.broker.tree.Intermediary
+import org.odfi.indesign.core.harvest.HarvestedResource
+
+trait PipelineStep extends Intermediary with HarvestedResource {
+  
+}
